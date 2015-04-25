b0944dcee3f964bb35740a61c9db43c45f9ba929
Luis Santos <l.santos@har.mrc.ac.uk>
2015-04-25 13:54:16 -0400

    week 2 programming assignment@

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment week 2
+##
+## write a pair of functions that cache the inverse of a matrix:
+##
+## - makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## - cacheSolve: This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above.
+##
+## If the inverse has already been calculated (and the matrix has not changed), then the
+## cachesolve should retrieve the inverse from the cache.
+## Computing the inverse of a square matrix can be done with the solve function in R.
+## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+##
+## For this assignment, assume that the matrix supplied is always invertible.
+##
 
-## Write a short comment describing this function
+
+## makeCacheMatrix(x = matrix())
+## Defining the setters and getters for the matrix; the setters allocate the variables
+## and assign them values; the getters retrieve those values
+## When done, return a list of setters and getters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL				# instantiate inverted matrix variable, empty
+	set <- function(z) {
+		message('[re]setting matrix; clearing cached data')
+		x <<- z
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(invrtd) inv <<- invrtd	# cache 'invrtd' in inv!
+	getinv <- function() inv
+	list(set = set, get = get,
+		 setinv = setinv,
+		 getinv = getinv)
 }
 
+## cacheSolve(lf)
+## Given the list of functions 'lf' returned from makeCacheMatrix, find
+## the cached inverted matrix result; if it does not exist, calculate it
+## and store it in cache.
+## NB: according to instructions, we are assuming that the matrix provided
+## is always invertible, thus no check / error catching is implemented
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(lf, ...) {
+	inv <- lf$getinv()
+	if(!is.null(inv)) {	# is 'inv' not null / is there cached data?
+		message("data found in cache")
+		return(inv)		# return means code will exit the function here
+	}
+	x <- lf$get()
+	invrtd <- solve(x)
+	lf$setinv(invrtd)
+	invrtd
 }
