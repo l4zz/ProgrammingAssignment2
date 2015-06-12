ab8f6e170b77ea7c803061a5448483112502eee5
Luis Santos <l.santos@har.mrc.ac.uk>
2015-06-12 20:49:19 +0100

    edit to some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,7 @@
 ## Programming Assignment week 2
-##
-## write a pair of functions that cache the inverse of a matrix:
-##
-## - makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-## - cacheSolve: This function computes the inverse of the special "matrix" returned by
-## makeCacheMatrix above.
-##
-## If the inverse has already been calculated (and the matrix has not changed), then the
-## cachesolve should retrieve the inverse from the cache.
-## Computing the inverse of a square matrix can be done with the solve function in R.
-## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
-##
-## For this assignment, assume that the matrix supplied is always invertible.
-##
 
-
-## makeCacheMatrix(x = matrix())
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##
 ## Defining the setters and getters for the matrix; the setters allocate the variables
 ## and assign them values; the getters retrieve those values
 ## When done, return a list of setters and getters.
@@ -35,12 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 		 getinv = getinv)
 }
 
-## cacheSolve(lf)
+## cacheSolve: This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above.
+##
 ## Given the list of functions 'lf' returned from makeCacheMatrix, find
 ## the cached inverted matrix result; if it does not exist, calculate it
 ## and store it in cache.
-## NB: according to instructions, we are assuming that the matrix provided
-## is always invertible, thus no check / error catching is implemented
+## No check / error catching implemented
 
 cacheSolve <- function(lf, ...) {
 	inv <- lf$getinv()
